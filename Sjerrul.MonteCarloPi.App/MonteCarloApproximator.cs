9835cde93bcd0700d412dd2e375f203ddd8753bc
Sjerrul <cerylwiltink@hotmail.com>
2014-06-02 09:20:20 +0200

    First commit, made the approximation working

---
+++ Sjerrul.MonteCarloPi.App/MonteCarloApproximator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sjerrul.MonteCarloPi.App
+{
+    public class MonteCarloApproximator
+    {
+        private Random _random = new Random();
+
+        public MonteCarloApproximator()
+        {
+
+        }
+
+        /// <summary>
+        /// Get's pi as determined by a Monte Carlo Approximation. The larger the number of iterations, the more time it takes but the more precise the result
+        /// </summary>
+        /// <param name="numberOfIterations">The number of iterations used in the approximation</param>
+        /// <returns></returns>
+        public double GetPi(int numberOfIterations)
+        {
+            Func<double, double, bool> PiLambda = (x, y) => { return Math.Pow(x, 2) + Math.Pow(y, 2) <= 1; };
+          
+            int hits = 0;
+            int iteration = 0;
+            while (iteration != numberOfIterations)
+            {
+                double x = GetRandomBetween(-1, 1);
+                double y = GetRandomBetween(-1, 1);
+
+                if (PiLambda.Invoke(x, y))
+                {
+                    hits++;
+                }
+
+                iteration++;
+            }
+
+            double rho = ((double)hits/numberOfIterations);
+            double pi = rho * 4;
+
+            return pi;
+        }
+
+        public double GetRandomBetween(double low, double high)
+        {
+            double offset = low - high;
+            return (_random.NextDouble() * high) + low;
+        }
+    }
+}
---
+++ Sjerrul.MonteCarloPi.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sjerrul.MonteCarloPi.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonteCarloApproximator approximator = new MonteCarloApproximator();
+
+            Console.WriteLine(String.Format("Pi = {0} ({1} iterations)", approximator.GetPi(1000), 1000));
+            Console.WriteLine(String.Format("Pi = {0} ({1} iterations)", approximator.GetPi(10000), 10000));
+            Console.WriteLine(String.Format("Pi = {0} ({1} iterations)", approximator.GetPi(100000), 100000));
+            Console.WriteLine(String.Format("Pi = {0} ({1} iterations)", approximator.GetPi(1000000), 1000000));
+
+            Console.ReadKey();
+
+        }
+    }
+}
